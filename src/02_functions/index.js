d97a9ae7b296b91bc786bc3513447280ebdf9e0e
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2025-04-12 17:59:18 +0300

    add: types, casting

---
+++ src/02_functions/index.js
@@ -0,0 +1,109 @@
+
+// ===== Function declaration
+
+// render();
+// function render() {
+//     console.log('test string');
+// }
+// render();
+
+
+
+// function render() {
+//     let val = 34;
+//     console.log(val);
+// }
+// render();
+
+
+
+// let email = 'vasia@mail.com';
+// function render() {
+//     let email = 'dima@mail.com';
+//     email = 'petya@mail.com';
+//     console.log(email);
+    
+// }
+// render();
+// console.log(email);
+
+
+
+
+// function render1(message) {
+//     console.log(message);
+// }
+// function render2(message = 'mo_message') {
+//     console.log(message);
+// }
+
+// render1('vasia');
+// render1();
+// render2();
+// render2('petya')
+// console.log(render1());
+
+
+
+
+// function getRandom() {
+//     return 4;
+// }
+// function render(val = getRandom()) {
+//     console.log(val);
+// }
+
+// render(23);
+// render();
+
+
+
+// function f() {
+//     console.log(1);
+// }
+// function f(val) {
+//     console.log(2);
+// }
+
+// f();
+
+
+
+
+// function f(message) {
+//     // if (message === undefined)
+//     //     console.log('error');
+//     // console.log(message);
+    
+//     // console.log(message ?? 'error');
+
+//     message ??= 'error';
+//     console.log(message);
+// }
+
+// f();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ===== Function expression
+
+
+
+// ===== Arrow function
+
+
